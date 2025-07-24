332c1a1f2109c421270d9ae8b46e2a00dbee8b9f
LeTrietHuan-Student <huan.le.cit23@eiu.edu.vn>
2025-07-24 14:56:31 +0700

    commit lab3

--- LAB3/PPYMK.java
+++ LAB3/PPYMK.java
@@ -1,5 +1,7 @@
 package LAB3;
 
+import LAB3.EIFOLTRE.InputReader;
+import LAB3.EIFOLTRE.Vertex;
 import java.io.*;
 import java.util.*;
 
@@ -8,9 +10,60 @@ public class EIFOLTRE {
     static InputReader sc = new InputReader(System.in);
     static StringBuilder sb = new StringBuilder();
 
+    static Map<Integer, TreeSet<Integer>> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        Vertex[] vertices = new Vertex[n];
+        for (int i = 0; i < n; i++) {
+            vertices[i] = new Vertex(i, false);
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            vertices[u].addNeighbor(vertices[v]);
+            vertices[v].addNeighbor(vertices[u]);
+        }
+
+        int member = sc.nextInt();
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+
+        }
+
+    }
+
+    static void bfs(Vertex startVertex, Vertex[] vertexs) {
+        Queue<Vertex> queue = new LinkedList<>();
+        queue.add(startVertex);
+        startVertex.level = 0;
+        startVertex.visited = true;
+
+        while (!queue.isEmpty()) {
+            Vertex w = queue.poll();
+
+            for (Vertex u : w.adj) {
+                if (!u.visited) {
+                    queue.add(u);
+                    u.visited = true;
+                    u.level += w.level + 1;
+                    if (!map.containsKey(w.level + 1)) {
+                        TreeSet<Integer> mySet = new TreeSet<>();
+                        map.put(w.level + 1, mySet);
+                    }
+                    TreeSet<Integer> myTreeSet = map.get(w.level + 1);
+                    myTreeSet.add(u.id);
+
+                }
+            }
+        }
+    }
+
     static class Vertex {
         int id;
         boolean visited;
+        int level;
         List<Vertex> adj = new ArrayList<>();
 
         public Vertex(int id, boolean visited) {
