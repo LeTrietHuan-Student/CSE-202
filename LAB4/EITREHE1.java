f81971b38439d3d28f118c4ab5b927516089572d
LeTrietHuan-Student <huan.le.cit23@eiu.edu.vn>
2025-07-31 14:25:29 +0700

    lab 4 ver1

---
+++ LAB4/EITREHE1.java
@@ -0,0 +1,106 @@
+package LAB4;
+
+import java.io.*;
+import java.util.*;
+
+public class EITREHE1 {
+
+    static InputReader sc = new InputReader(System.in);
+    static int MAX;
+    static int level;
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int m = n + 1;
+        Vertex[] vertices = new Vertex[n];
+        for (int i = 0; i < n; i++) {
+            vertices[i] = new Vertex(i);
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            vertices[u].addNeighbor(vertices[v]);
+            vertices[v].addNeighbor(vertices[u]);
+        }
+
+        dfs(vertices[0], level);
+
+        System.out.println(level);
+    }
+
+    static void dfs(Vertex v, int level) {
+        v.visited = true;
+        for (Vertex x : v.adj) {
+            if (!x.visited) {
+                dfs(x, level + 1);
+            }
+        }
+    }
+
+    static class Vertex {
+        int id;
+        int level;
+        boolean visited;
+        List<Vertex> adj = new ArrayList<>();
+
+        public Vertex(int id) {
+            this.id = id;
+        }
+
+        public void addNeighbor(Vertex v) {
+            adj.add(v);
+        }
+
+    }
+
+    // Input Reader
+    static class InputReader {
+
+        StringTokenizer tokenizer;
+        BufferedReader reader;
+        String token;
+        String temp;
+
+        public InputReader(InputStream stream) {
+            tokenizer = null;
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public InputReader(FileInputStream stream) {
+            tokenizer = null;
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String nextLine() throws IOException {
+            return reader.readLine();
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    if (temp != null) {
+                        tokenizer = new StringTokenizer(temp);
+                        temp = null;
+                    } else {
+                        tokenizer = new StringTokenizer(reader.readLine());
+                    }
+                } catch (IOException e) {
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+}
