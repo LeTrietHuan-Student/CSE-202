9b4e1a5db2a643e73857c985de111590c8648939
LeTrietHuan-Student <huan.le.cit23@eiu.edu.vn>
2025-08-01 23:38:39 +0700

    Update Treasure.java

--- LAB3/Treasure.java
+++ LAB3/Treasure.java
@@ -1,5 +1,202 @@
 package LAB3;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-public class Java {
+public class Treasure {
+    static InputReader sc;
+    static StringBuilder sb = new StringBuilder();
+    static int MAX = 0;
 
+    public static void main(String[] args) throws IOException {
+        sc = new InputReader(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        Vertex [] vertexs = new Vertex[n];
+        for (int i = 0; i < vertexs.length; i++) {
+             vertexs[i] = new Vertex(i);
+        }
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            vertexs[u].addNeighbor(vertexs[v]);
+            vertexs[v].isUnder = true;
+        }
+        int index = 0;
+        for (Vertex vertex : vertexs) {
+            if (!vertex.isUnder) {
+                   index  = vertex.id;
+                   break;   
+            }
+        }
+        dfs(vertexs[index],1);
+
+       
+        System.out.println(MAX);
+    }
+
+   
+
+    static void dfs(Vertex v, int level) {
+        v.visited = true;
+         MAX = Math.max(MAX, level);
+        for (Vertex w : v.adjacentVertices) {
+            if (!w.visited) {
+                dfs(w, level+1);
+            }
+        }
+
+    }
+
+    static class Vertex {
+        int id;
+        boolean isUnder;
+
+        boolean visited;
+
+        List<Vertex> adjacentVertices = new ArrayList<>();
+
+        public Vertex(int id) {
+            this.id = id;
+
+        }
+
+        public void addNeighbor(Vertex v) {
+            adjacentVertices.add(v);
+        }
+    }
+
+    static class InputReader {
+        private byte[] inbuf = new byte[2 << 23];
+        public int lenbuf = 0, ptrbuf = 0;
+        public InputStream is;
+
+        public InputReader(InputStream stream) throws IOException {
+
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = System.in;
+            lenbuf = is.read(inbuf);
+        }
+
+        public InputReader(FileInputStream stream) throws IOException {
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = stream;
+            lenbuf = is.read(inbuf);
+        }
+
+        public boolean hasNext() throws IOException {
+            if (skip() >= 0) {
+                ptrbuf--;
+                return true;
+            }
+            return false;
+        }
+
+        public String nextLine() throws IOException {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b) && b != ' ') { // when nextLine, ()
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b
+                // != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        private int readByte() {
+            if (lenbuf == -1)
+                throw new InputMismatchException();
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0)
+                    return -1;
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public Character nextChar() {
+            return skip() >= 0 ? (char) skip() : null;
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
 }
